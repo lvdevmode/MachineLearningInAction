d3d713fc1a345aeafe2639e99ad39fc162dd684e
lvdevmode <lvdevmode@gmail.com>
2018-04-08 01:21:58 +0530

    Analysis done.

--- ch2_knn_dating.py
+++ ch2_knn_dating.py
@@ -40,14 +40,39 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure()
 
-ax1 = fig.add_subplot(2, 1, 1)
+# Simple plot
+ax1 = fig.add_subplot(2, 2, 1)
 ax1.scatter(X[:, 1], X[:, 2])
 ax1.set_xlabel("Percentage of Time Spent Playing Video Games")
 ax1.set_ylabel("Liters of Ice-Cream Consumed Per Week")
 
-ax2 = fig.add_subplot(2, 1, 2)
-ax2.scatter(X[:, 1], X[:, 2], 15.0*np.array(y), 15.0*np.array(y))
+# Different color markers of different sizes for different labels.
+ax2 = fig.add_subplot(2, 2, 2)
+ax2.scatter(X[:, 1], X[:, 2], s=15.0*np.array(y), c=y)
 ax2.set_xlabel("Percentage of Time Spent Playing Video Games")
 ax2.set_ylabel("Liters of Ice-Cream Consumed Per Week")
+
+# Simply providing labels like this doesn't really work. See for yourself by un-commenting #58.
+ax3 = fig.add_subplot(2, 2, 3)
+ax3.scatter(X[:, 1], X[:, 0], label=y, c=y)
+#ax3.legend()
+ax3.set_xlabel("Percentage of Time Spent Playing Video Games")
+ax3.set_ylabel("Number of Frequent Flyer Miles Earned Per Year")
+
+# In python, if you need a proper legend, class-wise, then you need to
+# plot the points class-wise. And then the options open up. 
+ax4 = fig.add_subplot(2, 2, 4)
+possible_labels = np.unique(np.array(y))
+#markers = ['^', 's', 'o']
+X_class = []
+for label in possible_labels:
+    for i in range(len(y)):
+        if y[i] == label :
+            X_class.append(X[i])
+    X_class = np.array(X_class)
+    #ax4.scatter(X_class[:, 2], X_class[:, 0], marker=markers[label - 1], label=label)
+    ax4.scatter(X_class[:, 2], X_class[:, 0], label=label)
+    X_class = []
+ax4.legend()
 
 plt.show()
