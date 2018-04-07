6aafaefa55beca883c8c8840fad7bb068db1219c
lvdevmode <lvdevmode@gmail.com>
2018-04-07 13:49:50 +0530

    Analysis begins.

--- ch2_knn_dating.py
+++ ch2_knn_dating.py
@@ -9,7 +9,10 @@ Created on Tue Apr  3 16:52:17 2018
 import numpy as np
 import operator
 
-#filename = "./data/datingTestSet.txt"
+# Parsing the input text file
+
+## filename = "./data/datingTestSet.txt"
+## filename = "./data/datingTestSet_withNumericLabels.txt"
 
 def file2matrix(filename):
     f = open(filename, 'r')
@@ -21,7 +24,30 @@ def file2matrix(filename):
         line = line.strip()
         elements = line.split('\t')
         X.append(elements[0:3])
-        y.append(elements[3])
+        y.append(int(elements[-1]))
     
     X = np.array(X)
     return X, y
+
+
+# Analyzing the input / Visualization
+ 
+X, y = file2matrix("./data/datingTestSet_withNumericLabels.txt")
+## X = [Number of Frequent Flyer Miles Earned Per Year, Percentage of Time Spent Playing Video Games,
+## Liters of Ice-Cream Consumed Per Week], y = [Degree of Likenesss]
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(2, 1, 1)
+ax1.scatter(X[:, 1], X[:, 2])
+ax1.set_xlabel("Percentage of Time Spent Playing Video Games")
+ax1.set_ylabel("Liters of Ice-Cream Consumed Per Week")
+
+ax2 = fig.add_subplot(2, 1, 2)
+ax2.scatter(X[:, 1], X[:, 2], 15.0*np.array(y), 15.0*np.array(y))
+ax2.set_xlabel("Percentage of Time Spent Playing Video Games")
+ax2.set_ylabel("Liters of Ice-Cream Consumed Per Week")
+
+plt.show()
